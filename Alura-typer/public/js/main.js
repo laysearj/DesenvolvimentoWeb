e955874889f8f9df4097f8bf69d72d096366ab01
layse araújo <layse.araujo.lr@gmail.com>
2020-04-15 17:06:59 -0300

    Introdução jQuery

---
+++ Alura-typer/public/js/main.js
@@ -0,0 +1,96 @@
+// $ = jQuerry
+var tempoInicial =  $("#tempo-digitacao").text();
+var campo = $(".campo-digitacao");
+
+$(document).ready(function(){
+
+    atualizaTamanhoFrase();
+    inicializaContadores();
+    inicializaCronometro();
+    inicializaMarcadores();
+    $("#botao-reiniciar").click(reiniciaJogo);
+
+});
+
+
+
+function atualizaTamanhoFrase() {
+    
+    var frase = $(".frase").text(); //procura e guarde a frase
+    var numPalavras = frase.split(" ").length; //função split separa com o elemento que eu coloco dentro
+    var tamanhoFrase = $("#tamanho-frase"); //ta achando o lugar desse id
+    tamanhoFrase.text(numPalavras); //altera o id
+
+}
+
+
+function inicializaContadores() {
+    
+    //função faça se estiver escrevendo -> dentro da função
+    campo.on("input", function(){
+        var conteudo = campo.val();//valor dentro do meu campo
+        var qtdPalavras = conteudo.split(/\S+/).length-1; // S = space
+        $("#contador-palavras").text(qtdPalavras);
+
+        var qtdCaracteres = conteudo.length;
+        $("#contador-caracteres").text(qtdCaracteres);
+    });
+
+}
+
+
+function inicializaCronometro() {
+
+    var tempoRestante = $("#tempo-digitacao").text();
+    //focus->quando entra no campo para digitar
+    campo.one("focus", function(){
+        var cronometroId = setInterval(function(){
+            tempoRestante--;
+            $("#tempo-digitacao").text(tempoRestante);
+            if(tempoRestante < 1){
+                clearInterval(cronometroId);//trava a função SetInterval
+                finalizaJogo();
+            }
+        }, 1000);
+    })
+
+}
+
+function inicializaMarcadores() {
+
+    var frase = $(".frase").text();
+    campo.on("input", function() {
+        var digitado = campo.val();
+        var comparavel = frase.substr(0 , digitado.length);
+
+        if(digitado == comparavel) {
+            campo.addClass("borda-verde");
+            campo.removeClass("borda-vermelha");
+        } else {
+            campo.addClass("borda-vermelha");
+            campo.removeClass("borda-verde");
+        }
+    });
+
+}
+
+
+function reiniciaJogo(){
+
+    campo.attr("disabled", false);
+    campo.val("");
+    $("#contador-palavras").text("0");
+    $("#contador-caracteres").text("0");
+    $("#tempo-digitacao").text(tempoInicial);
+    inicializaCronometro();
+    campo.toggleClass("campo-desativado");
+    campo.removeClass("borda-vermelha"); 
+    campo.removeClass("borda-verde"); 
+
+}
+
+function finalizaJogo() {
+    campo.attr("disabled", true);
+    campo.toggleClass("campo-desativado");
+    inserePlacar();
+}
---
+++ Alura-typer/public/js/placar.js
@@ -0,0 +1,43 @@
+function inserePlacar(){
+    var corpoTabela = $(".placar").find("tbody");
+    var usuario = "Layse";
+    var numPalavras = $("#contador-palavras").text();
+
+    var linha = novaLinha(usuario, numPalavras);
+    linha.find(".botao-remover").click(removeLinha);
+
+    corpoTabela.prepend(linha);
+}
+
+function novaLinha(usuario, numPalavras) {
+
+    var linha = $("<tr>");
+    var colunaUsuario = $("<td>").text(usuario);
+    var colunaPalavras = $("<td>").text(numPalavras);
+    var colunaRemover = $("<td>");
+
+    var link = $("<a>").attr("href","#").addClass("botao-remover");
+    var icone = $("<i>").addClass("small").addClass("material-icons").text("delete");
+
+    // Icone dentro do <a>
+    link.append(icone);
+
+    // <a> dentro do <td>
+    colunaRemover.append(link);
+
+    // Os três <td> dentro do <tr>
+    linha.append(colunaUsuario);
+    linha.append(colunaPalavras);
+    linha.append(colunaRemover);
+
+    return linha;
+
+}
+
+function removeLinha(event) {
+
+    event.preventDefault();
+    $(this).parent().parent().remove();
+
+
+}
